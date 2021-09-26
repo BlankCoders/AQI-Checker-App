15a13bb48394deee5eddd88b3747efbd7c1a048c
lostgirljourney <falgunisarkar526@gmail.com>
2021-09-26 19:43:32 +0530

    commit

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -6,22 +6,37 @@ import "./Details.css";
 function Details(props) {
 	//   return <div>{props.val}</div>;
 	console.log(props);
-
-	let AQIIndex = props.Pval.main.aqi - 1;
-	return (
-		<>
+	if (props.okay === false) {
+		return (
 			<div className="details">
-				<p className="details-loc">
-					{props.Wval.name}, {props.Wval.sys.country}
-				</p>
-				<img src={AQIData.main[AQIIndex].image} alt="" width="100px" />
-				<p className="details-AQI">Air Quality Index:</p>
-				<p className="details-AQIValue">{AQIData.main[AQIIndex].AQI}</p>
+				<p className="details-loc">City Not Found</p>
+				<img src={AQIData.err.image} alt="" />
+				<Advice okay={props.okay} />
 			</div>
-			<Bullet Pval={props.Pval} temp={props.Wval.main.temp} />
-			<Advice okay={props.okay} AQIIndex={AQIIndex} />
-		</>
-	);
+		);
+	} else {
+		let AQIIndex = props.Pval.main.aqi - 1;
+		return (
+			<>
+				<div className="details">
+					<p className="details-loc">
+						{props.Wval.name}, {props.Wval.sys.country}
+					</p>
+					<img
+						src={AQIData.main[AQIIndex].image}
+						alt=""
+						width="100px"
+					/>
+					<p className="details-AQI">Air Quality Index:</p>
+					<p className="details-AQIValue">
+						{AQIData.main[AQIIndex].AQI}
+					</p>
+				</div>
+				<Bullet Pval={props.Pval} temp={props.Wval.main.temp} />
+				<Advice okay={props.okay} AQIIndex={AQIIndex} />
+			</>
+		);
+	}
 }
 
 export default Details;
